b548e69780f9fb63bf95008816c30d4b82e634d8
jaivl <86610376+jaivl@users.noreply.github.com>
2021-11-12 12:59:39 +0100

    Add files via upload

--- graficador.py
+++ graficador.py
@@ -1,6 +1,39 @@
 # Autor: Jairo Valea LÃ³pez
+
+# Documentación:
+
+# ensayos_disipacion(datos,inicio='01/07/2021',fin='31/10/2021',rangos)
+# función experimental no completada
+
+# visib(datos,inicio,fin,rangos,rampa)
+# Grafica las funciones de densidad granulométricas entre las fechas señaladas,
+# con los rangos de visibilidad aportados.
+# 'rampa' hace referencia a la rampa de color
+#   - 'ensayo' - un color para cada ensayo
+#   - 'temp' - gradiente de color por temperatura
+#   - 'tempdisc' - un color para cada temperatura
+#   - 'mes' - un color para cada mes
+
+# separar_ensayos(datos, inicio, fin, rangos, niebla, ensayos)
+# Similar a visib, pero en vez de hacer una gráfica por cada rango de
+# visibilidad extrae una gráfica destacando cada ensayo.
+# 'niebla' y 'ensayos son valores booleanos:
+#   - para extraer solo los 8., niebla = True y ensayos = False
+#   - para extraer los 8. + 10 primeros min de niebla, niebla = True y 
+#       ensayos = True
+#   - para extraer los 10., niebla = False y ensayos = True
+
+# distribuciones_cajas(datos,ensayos = '8.',inicio,fin,rangos)
+# Extrae los boxplot caracterizadores de la niebla
+# 'ensayos' es un argumento cadena que puede tomar múltiples valores:
+#   - '8.' para usar solo los ensayos de caracterización
+#       - '8. + cs12 t=8'
+#   - '10' para usar solo los ensayos de disipación
+#   - 'cs' para usar solo los ensayos de disipación con cloruro sódico
 #
-# psÃ©
+#   - 'solo b28 min 7' para representar solo un minuto concreto de un ensayo
+#
+
 
 # Importado de librerÃ­as habituales
 
@@ -476,7 +509,7 @@ def distribuciones_cajas(datos, ensayos = '8.', inicio = '01/06/2021', fin = '31
 #######################
 
 def filtrar_corregida(datos,rangos):
-    lim = [15,15,15,15,25,25,50]
+    lim = [15,15,15,15,25,25,50,50,50]
     for v in range(len(rangos)-1):
         datos.drop(datos[(datos['Visibilidad corregida (m)'] >= rangos[v]) &
             (datos['Visibilidad corregida (m)'] < rangos[v+1]) &
@@ -512,8 +545,8 @@ def ensayos_only(datos,rangos,corregido=False):
     return datos
 
 def preparar(datos,inicio='01/06/2021',fin='31/07/2021',rangos=[],
-             soloniebla=False, ensayos=False):
-    if soloniebla:
+             niebla=False, ensayos=False):
+    if niebla:
         if ensayos == False:
             datos = caract_only(datos,rangos,corregido=True)
         else:
@@ -530,7 +563,7 @@ def preparar(datos,inicio='01/06/2021',fin='31/07/2021',rangos=[],
     datos.drop(datos[datos['Prec_mensual'] == -9999].index, inplace=True)
     return datos
 
-def separar_ensayos(datos, inicio, fin, rangos=[],soloniebla=True):
+def separar_ensayos(datos, inicio, fin, rangos=[],niebla=True,ensayos=True):
     if rangos:
         pass
     else: 
@@ -538,7 +571,7 @@ def separar_ensayos(datos, inicio, fin, rangos=[],soloniebla=True):
             rangos.append(round(np.quantile(datos['Visibilidad corregida (m)'],v/6),1))
             rangos.append(2000)
             
-    datos = preparar(datos,inicio,fin,rangos,soloniebla)
+    datos = preparar(datos,inicio,fin,rangos,niebla,ensayos)
             
     diams_g = np.array([2.13,2.289,2.46,2.643,2.841,3.053,3.28,3.525,3.788,4.071,4.374,4.701,5.051,5.428,5.833,6.268,6.736,7.239,
                    7.779,8.359,8.983,9.653,10.373,11.147,11.979,12.872,13.833,14.865,15.974,17.165,18.446])
@@ -809,53 +842,174 @@ def ensayos_disipacion(datos, inicio, fin, rangos=[]):
     acumulado = (acumulado.T/acumulado[:,-1]).T
     
     for i in datos.Ensayo.unique():
-        fig = plt.figure(figsize = (16,9))
-        gs = gridspec.GridSpec(3, 3, height_ratios=[1,1,1])
-        ax15 = plt.subplot(gs[0,0])
-        ax30 = plt.subplot(gs[0,1])
-        ax45 = plt.subplot(gs[0,2])
-        ax60 = plt.subplot(gs[1,0])
-        ax75 = plt.subplot(gs[1,1])
-        ax100 = plt.subplot(gs[1,2])
-        ax200 = plt.subplot(gs[2,0])
+        fig = plt.figure(figsize = (16,7))
+        gs = gridspec.GridSpec(2, 2, width_ratios=[1,1])
+        eje = plt.subplot(gs[0,0])
+        non = plt.subplot(gs[1,0])
+        comp = plt.subplot(gs[0:2,1])
         
-        ejes = [ax15,ax30,ax45,ax60,ax75,ax100,ax200]
+        ejes = [eje,non,comp]
         labels = [2,3,4,5,6,8,10,12,15,18]
-        c = 0
 
         plt.suptitle('Ensayo ' + i + ' - ' +pd.DatetimeIndex(datos
         [datos['Ensayo'] == i].Hora).strftime('%d/%m, %H:%M')[0], size=16)
         
-        for eje in ejes:
-            ploteo = unidades[(datos['Visibilidad corregida (m)'] >= rangos[c])
-                            & (datos['Visibilidad corregida (m)'] < rangos[c+1])]
-            nieblainic = unidades[(datos['Visibilidad corregida (m)'] >= rangos[c])
-                            & (datos['Visibilidad corregida (m)'] < rangos[c+1]) &
-                            (datos['Ensayo'] == i) & (datos['Tiempo (min)'] < 9.5)]
-            durante = unidades[(datos['Visibilidad corregida (m)'] >= rangos[c])
-                            & (datos['Visibilidad corregida (m)'] < rangos[c+1]) &
-                            (datos['Ensayo'] == i) & (datos['Tiempo (min)'] > 9.5)
-                            & (datos['Tiempo (min)'] < 29.5)]
+        e_ploteo = unidades_norm[(datos['Visibilidad corregida (m)'] >= 10)
+                        & (datos['Visibilidad corregida (m)'] < 150)]
+        e_nie = unidades_norm[(datos['Visibilidad corregida (m)'] >= 10)
+                        & (datos['Visibilidad corregida (m)'] < 150) &
+                        (datos['Ensayo'] == i) & (datos['Tiempo (min)'] < 9.5)]
+        pe_inic = np.mean(e_nie,axis=0)
+        e_dur = unidades_norm[(datos['Visibilidad corregida (m)'] >= 10)
+                        & (datos['Visibilidad corregida (m)'] < 150) &
+                        (datos['Ensayo'] == i) & (datos['Tiempo (min)'] > 9.5)
+                        & (datos['Tiempo (min)'] < 29.5)]
+        pe_durante = np.mean(e_dur,axis=0)
+        e_post = unidades_norm[(datos['Visibilidad corregida (m)'] >= 10)
+                        & (datos['Visibilidad corregida (m)'] < 150) &
+                        (datos['Ensayo'] == i) & (datos['Tiempo (min)'] > 29.5)]
+        pe_post = np.mean(e_post,axis=0)
+        
+        n_ploteo = unidades[(datos['Visibilidad corregida (m)'] >= 10)
+                        & (datos['Visibilidad corregida (m)'] < 150)]
+        n_nie = unidades[(datos['Visibilidad corregida (m)'] >= 10)
+                        & (datos['Visibilidad corregida (m)'] < 150) &
+                        (datos['Ensayo'] == i) & (datos['Tiempo (min)'] < 9.5)]
+        pn_inic = np.mean(e_nie,axis=0)
+        n_dur = unidades[(datos['Visibilidad corregida (m)'] >= 10)
+                        & (datos['Visibilidad corregida (m)'] < 150) &
+                        (datos['Ensayo'] == i) & (datos['Tiempo (min)'] > 9.5)
+                        & (datos['Tiempo (min)'] < 29.5)]
+        pn_durante = np.mean(e_dur,axis=0)
+        n_post = unidades[(datos['Visibilidad corregida (m)'] >= 10)
+                        & (datos['Visibilidad corregida (m)'] < 150) &
+                        (datos['Ensayo'] == i) & (datos['Tiempo (min)'] > 29.5)]
+        pn_post = np.mean(e_post,axis=0)
+        
+        nieblainic = acumulado[(datos['Visibilidad corregida (m)'] >= 10)
+                        & (datos['Visibilidad corregida (m)'] < 150) &
+                        (datos['Ensayo'] == i) & (datos['Tiempo (min)'] < 9.5)]
+        durante = acumulado[(datos['Visibilidad corregida (m)'] >= 10)
+                        & (datos['Visibilidad corregida (m)'] < 150) &
+                        (datos['Ensayo'] == i) & (datos['Tiempo (min)'] > 9.5)
+                        & (datos['Tiempo (min)'] < 29.5)]
+        post = acumulado[(datos['Visibilidad corregida (m)'] >= 10)
+                        & (datos['Visibilidad corregida (m)'] < 150) &
+                        (datos['Ensayo'] == i) & (datos['Tiempo (min)'] > 29.5)]
+        
+        eje.set_ylim(0,0.5); non.set_ylim(0,120)
+        eje.set_ylabel('dN/N/dx'); non.set_ylabel('N * dN/N/dx')
+        
+        for a in ejes[0:2]:
+            a.set_title('Granulometría - visibilidad de 10 a 150 m')
+            a.set_xscale('log'); eje.grid(which = 'both')
+            a.set_xlim(2,18);
+            a.set_xlabel('Diámetro ($\mu$m)');
+            a.set_xticks(labels)
+            a.set_xticklabels(labels)
             
-            eje.set_title('Visibilidad de ' + str(rangos[c]) + ' a ' +
-                          str(rangos[c+1]) + ' m')
-            eje.set_xscale('log'); eje.grid(which = 'both')
-            eje.set_xlim(2,18);
-            eje.set_xlabel('Diámetro ($\mu$m)'); eje.set_ylabel('N * dN/N/dx');
-            eje.set_xticks(labels); eje.set_xticklabels(labels)
-            # for j in range(ploteo.shape[0]):
-            #     eje.plot(diams_g,ploteo[j],
-            #         c = 'black',alpha = 0.1, lw = 0.5)
-            for j in range(nieblainic.shape[0]):
-                eje.plot(diams_g,nieblainic[j],
-                    c = 'black', alpha = 0.6, lw = 0.75)
-            for j in range(durante.shape[0]):
-                eje.plot(diams_g,durante[j],
-                    c = 'blue', alpha = 0.6, lw = 0.75)
-            c = c+1
-        plt.tight_layout()
-        plt.savefig(ruta_proces + 'Gráficos/Ensayos_disipacion/' + i + '_sinnorm.png')
+        for j in range(e_ploteo.shape[0]):
+            eje.plot(diams_g,e_ploteo[j],
+                c = 'black',alpha = 0.05, lw = 0.5)
+        for j in range(e_nie.shape[0]):
+            eje.plot(diams_g,e_nie[j],
+                c = 'blue',alpha = 0.5, lw = 0.5)
+        for j in range(e_dur.shape[0]):
+            eje.plot(diams_g,e_dur[j],
+                c = 'red',alpha = 0.5, lw = 0.5)
 
+        for j in range(n_ploteo.shape[0]):
+            non.plot(diams_g,n_ploteo[j],
+                c = 'black',alpha = 0.05, lw = 0.5)
+        for j in range(n_nie.shape[0]):
+            non.plot(diams_g,n_nie[j],
+                c = 'blue',alpha = 0.5, lw = 0.5)
+        for j in range(n_dur.shape[0]):
+            non.plot(diams_g,n_dur[j],
+                c = 'red',alpha = 0.5, lw = 0.5)
+        
+        comp.set_title('Comparación de variables durante el ensayo')
+        
+        
+        comp.plot(nieblainic['Tiempo (min)'], 1-nieblainic[:,21],
+                  c = 'blue', lw = 1)
+        comp.plot(durante['Tiempo (min)'], 1-durante[:,21],
+                  c = 'red', lw = 1)
+        comp.plot(post['Tiempo (min)'], 1-post[:,21],
+                  c = 'blue', lw = 1)
+        
+        
+        plt.tight_layout()
+        plt.savefig(ruta_proces + 'Gráficos/Ensayos_disipacion/' + i + '_norm.png')
+
+    return
+
+def valores_tipicos(datos,inicio='01/06/2021',fin='30/09/2021',rangos=[]):
+    
+    datos = preparar(datos,inicio,fin,rangos,niebla=True,ensayos=True)
+            
+    diams_g = np.array([2.13,2.289,2.46,2.643,2.841,3.053,3.28,3.525,3.788,
+                        4.071,4.374,4.701,5.051,5.428,5.833,6.268,6.736,7.239,
+                        7.779,8.359,8.983,9.653,10.373,11.147,11.979,12.872,
+                        13.833,14.865,15.974,17.165,18.446
+                        ])
+    diams = np.array([0.104,0.111,0.12,0.129,0.138,0.149,0.16,0.172,0.184,
+                      0.198,0.213,0.229,0.246,0.264,0.284,0.305,0.328,0.352,
+                      0.379,0.407,0.437,0.47,0.505,0.543,0.583,0.627,0.674,
+                      0.724,0.778,0.836,0.898,0.965,1.037,1.115,1.198,1.287,
+                      1.383,1.486,1.597,1.717,1.845,1.982,2.13,2.289,2.46,
+                      2.643,2.841,3.053,3.28,3.525,3.788,4.071,4.374,4.701,
+                      5.051,5.428,5.833,6.268,6.736,7.239,7.779,8.359,8.983,
+                      9.653,10.373,11.147,11.979,12.872,13.833,14.865,15.974,
+                      17.165,18.446
+                      ])
+    dx = np.array([0.007,0.008,0.009,0.009,0.01,0.011,0.011,0.012,0.013,0.014,
+                   0.015,0.016,0.018,0.019,0.02,0.022,0.024,0.025,0.027,0.029,
+                   0.031,0.034,0.036,0.039,0.042,0.045,0.048,0.052,0.056,0.06,
+                   0.065,0.069,0.075,0.08,0.086,0.093,0.099,0.107,0.115,0.123,
+                   0.133,0.143,0.153,0.165,0.177,0.19,0.204,0.22,0.236,0.254,
+                   0.272,0.293,0.315,0.338,0.363,0.39,0.42,0.451,0.484,0.521,
+                   0.559,0.601,0.646,0.694,0.746,0.802,0.862,0.926,0.995,1.069,
+                   1.149,1.235,1.327
+                   ])
+    
+    vols = (np.pi/6000000)*(diams_g**3)
+    volsfinos = (np.pi/6000000)*(diams**3)
+
+    for v in range(len(rangos)-1):
+        vis = datos[(datos['Visibilidad corregida (m)'] >= rangos[v]) &
+        (datos['Visibilidad corregida (m)'] < rangos[v+1])]
+        
+        if (len(datos) > 0):
+            suma_gruesos = np.empty((len(vis),1))
+            suma_finos = np.empty((len(vis),1))
+            unidades = np.array(vis.iloc[:,57:88])
+            masas = np.array(vis.iloc[:,57:88])
+            masasfinas = np.array(vis.iloc[:,15:57])
+            for k in range(unidades.shape[0]):
+                masas[k,:] = vols * masas[k,:]
+                masasfinas[k,:] = volsfinos[0:42] * masasfinas[k,:]
+                suma_gruesos[k] = np.sum(unidades[k,:])
+                suma_finos[k] = np.sum(vis.iloc[k,15:57])
+            for m in range(unidades.shape[1]):
+                unidades[:,m] = np.divide(unidades[:,m],dx[m+42])
+                masas[:,m] = np.divide(masas[:,m],dx[m+42])
+        acumulado = np.cumsum(unidades,axis=1)
+        #acumulado = (acumulado.T/acumulado[:,-1]).T
+        
+        print('LWC',rangos[v],'a',rangos[v+1],'m:',round(
+            np.mean(vis['LWC (g/m3)']),5))
+        print('N(finas)',rangos[v],'a',rangos[v+1],'m:',round(
+            np.mean(suma_finos),2),'- sigma',round(np.std(suma_finos),2))
+        print('N(gruesas)',rangos[v],'a',rangos[v+1],'m:',round(
+            np.mean(suma_gruesos),2),'- sigma',round(np.std(suma_gruesos),2))
+        print('g/m3 finas',rangos[v],'a',rangos[v+1],'m:',round(
+            np.mean(masasfinas),5))
+        print('g/m3 gruesas',rangos[v],'a',rangos[v+1],'m:',round(
+            np.mean(masas),5))
+        print('ug por p. gruesa',rangos[v],'a',rangos[v+1],'m:',round(
+            10000000*np.mean(masas)/np.mean(suma_gruesos),6),'\n')
+        
     return
 
 ########################
@@ -868,11 +1022,13 @@ ruta_proces = 'C:\\Users\\miguel.anton\\Desktop\\NIEBLA\\Ensayos procesados\\'
 datos = pd.read_csv(ruta_proces + 'database_modif.csv', delimiter = ";", decimal = ".")
 datos.dropna(inplace=True)
 
-rang = [15,30,45,60,75,100,200,1000]
+rang = [15,30,45,60,75,100,200,1000,1500,2000]
 
-ensayos_disipacion(datos,inicio='01/06/2021',fin='30/09/2021',rangos=rang)
+#ensayos_disipacion(datos,inicio='01/07/2021',fin='31/10/2021',rangos=rang)
 
-#visib(datos,inicio='01/06/2021',fin='30/06/2021',rangos=rang,rampa='temp')
+#visib(datos,inicio='01/06/2021',fin='30/09/2021',rangos=rang,rampa='ensayo')
 
 #datos=distribuciones_cajas(datos,ensayos = '8.',inicio='20/08/2021',
 #                           fin='31/08/2021',rangos=rang)
+
+valores_tipicos(datos,inicio='01/06/2021',fin='30/09/2021',rangos=rang)
